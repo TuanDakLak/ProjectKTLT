455204da8961919433fce74e6cf62350c88aeccf
caoty113 <166032408+caoty113@users.noreply.github.com>
2024-06-07 23:23:47 +0700

    Add files via upload

---
+++ function.h
@@ -0,0 +1,83 @@
+#ifndef fucntion_h
+#define fucntion_h
+#include <iostream>
+using namespace std;
+#include <string.h>
+#include <string>
+struct Date {
+    int ngay, thang, nam;
+};
+struct User {
+    string id;
+    string password;
+    string lastName;
+    string firstName;
+    string className;
+    string gender;
+    bool isStaff;
+    User* prev;
+    User* next;
+};
+struct Course {
+    string id;
+    string courseName;
+    string teacherName;
+    int max = 50;
+    Course* prev;
+    Course* next;
+};
+struct ListCourses {
+    Course* head;
+    Course* tail;
+    Date startDate, endDate;
+    int size=0;
+};
+struct ListUser {
+    User* head;
+    User* tail;
+    int size;
+};
+struct Student {
+    string studentID;
+    string lastName;
+    string firstName;
+    string gender;
+    string socialID;
+    Date dateOfBirth;
+    int academicYear;
+    ListCourses enrolledCourses;
+    Student* prev;
+    Student* next;
+};
+struct ListStudent {
+    Student* head;
+    Student* tail;
+    string className;
+    string year;
+};
+struct Class {
+    string className;
+    Class* prev;
+    Class* next;
+};
+struct ListClasses {
+    Class* head;
+    Class* tail;
+    int size;
+};
+struct Semester {
+    int semester;
+    Date startDate, endDate;
+};
+struct Year {
+    int From, To;
+};
+void addStudent(ListStudent& list, Student* x);
+void viewOfCourse(ListCourses list);
+
+void updateCourseIf(ListCourses& List);
+
+#endif // !1
+
+
+

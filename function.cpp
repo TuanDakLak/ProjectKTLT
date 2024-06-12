063c5f99e3498f0af861587a759f18c4a3f98b6d
caoty113 <166032408+caoty113@users.noreply.github.com>
2024-06-12 15:41:52 +0700

    Add files via upload

--- function.cpp
+++ function.cpp
@@ -1,22 +1,159 @@
-#include "function.h"
+
+
+#include <iostream>
+using namespace std;
+
+#include "fstream"
+#include <string>
+struct Date {
+	int ngay, thang, nam;
+};
+struct User {
+	string id;
+	string password;
+	string lastName;
+	string firstName;
+	string className;
+	string gender;
+	bool isStaff;
+	User* prev;
+	User* next;
+};
+struct Course {
+	string id;
+	string courseName;
+	string teacherName;
+	int max = 50;
+	int credits;
+	int dayOfWeek;
+	string session;
+	Course* prev;
+	Course* next;
+};
+struct ListCourses {
+	Course* head;
+	Course* tail;
+	Date startDate, endDate;
+	int size = 0;
+};
+struct ListUser {
+	User* head;
+	User* tail;
+	int size;
+};
+struct Student {
+	int NO;
+	string studentID;
+	string lastName;
+	string firstName;
+	string gender;
+	string socialID;
+	Date dateOfBirth;
+	int academicYear;
+	ListCourses enrolledCourses;
+	Student* prev;
+	Student* next;
+};
+struct ListStudent {
+	Student* head;
+	Student* tail;
+	string className;
+	string year;
+};
+struct Class {
+	ListStudent list;
+	string className;
+	Class* prev;
+	Class* next;
+};
+struct ListClasses {
+	Class* head;
+	Class* tail;
+	int size;
+};
+struct Year {
+	int From, To;
+	ListClasses listclasses;
+};
+struct Semester {
+	int semester;
+	Date startDate, endDate;
+	ListCourses list;
+};
+
+void nhapngay(Student*& s)
+{
+	cout << "Input date: ";
+	cin >> s->dateOfBirth.ngay;
+	cout << "Input month: ";
+	cin >> s->dateOfBirth.thang;
+	cout << "Input year: ";
+	cin >> s->dateOfBirth.nam;
+}
+Student* khoitaosinhvien()
+{
+	Student* s = new Student();
+
+	cout << "Enter NO: ";
+	cin >> s->NO;
+	cin.ignore();
+	cout << "Enter student id: ";
+	getline(cin, s->studentID, '\n');
+	cout << "Enter last name: ";
+	getline(cin, s->lastName, '\n');
+	cout << "Enter first name: ";
+	getline(cin, s->firstName, '\n');
+	cout << "Enter gioi tinh: ";
+	getline(cin, s->gender, '\n');
+	cout << "Enter social id: ";
+	getline(cin, s->socialID, '\n');
+	nhapngay(s);
+	s->next = NULL;
+	return s;
+}
+
+bool checkTime(Date a,Date b) {
+	if (a.thang < b.thang) return false;
+	else if (a.thang>b.thang) return true;
+	else if (a.thang == b.thang) {
+		if (a.ngay < b.ngay) return false;
+		return true;
+	}
+	
+}
 Semester createSemester() {
 	Semester tmp;
 	cout << "Hoc ki bao nhieu (1,2 hay 3) : "; cin >> tmp.semester;
 	cout << "Nam hoc :"; cin >> tmp.startDate.nam; tmp.endDate.nam = tmp.startDate.nam;
-	cout << "Ngay bat dau : ";
-	cout << "\t\t\tNgay : ";
+	cout << "Ngay bat dau : " << endl;;
+	cout << "\t\tNgay : ";
 	cin >> tmp.startDate.ngay;
-	cout << "\t\t\tThang : ";
+	cout << "\t\tThang : ";
 	cin >> tmp.startDate.thang;
-	cout << "Ngay ket thuc : ";
-	cout << "\t\t\tNgay : ";
+	cout << "Ngay ket thuc : "<<endl;
+	cout << "\t\tNgay : ";
 	cin >> tmp.endDate.ngay;
-	cout << "\t\t\tThang : ";
+	cout << "\t\tThang : ";
 	cin >> tmp.endDate.thang;
+	while (checkTime(tmp.startDate, tmp.endDate)) {
+		cout << "Thoi gian khong hop le!!Vui long nhap lai... \n";
+		cout << "Ngay bat dau : " << endl;;
+		cout << "\t\tNgay : ";
+		cin >> tmp.startDate.ngay;
+		cout << "\t\tThang : ";
+		cin >> tmp.startDate.thang;
+		cout << "Ngay ket thuc : " << endl;
+		cout << "\t\tNgay : ";
+		cin >> tmp.endDate.ngay;
+		cout << "\t\tThang : ";
+		cin >> tmp.endDate.thang;
+	}
+	tmp.list.head = tmp.list.tail = NULL;
 	return tmp;
 }
 Course* CreateCourse() {
 	Course* p = new Course();
+	cin.ignore();
 	cout << "ID mon hoc : ";
 	getline(cin, p->id);
 	cout << "Ten mon hoc : ";
@@ -41,14 +178,24 @@ Course* CreateCourse() {
 	cout << "\t\t\tchon 6: Thu sau\n";
 	cout << "\t\t\tchon 7: Thu bay\n";
 	cout << "Hoc vao thu : "; cin >> p->dayOfWeek;
+	cin.ignore();
 	cout << "Hoc vao luc : "; getline(cin, p->session);
 	return p;
 }
 void addCourseToSemester(Semester& hk) {
 	Course* monHoc = CreateCourse();
+	ofstream file;
+	file.open("./Course/" + monHoc->id + ".csv", ios::out );
+	if (!file.is_open()) {
+		cout << "errol\n";
+		return;
+	}
+	file << "ID,LAST NAME,FIST NAME,DATE OF BIRT"<<endl;
 	if (monHoc == NULL) return;
+	
 	if (hk.list.head == NULL) {
 		hk.list.head = hk.list.tail = monHoc;
+		
 		return;
 	}
 	Course* p = hk.list.head;
@@ -57,6 +204,9 @@ void addCourseToSemester(Semester& hk) {
 		p = p->next;
 	}
 	p->next = monHoc;
+
+
+	//file.close();
 }
 
 
@@ -90,10 +240,29 @@ void viewOfCourse(ListCourses list) {
 	}
 }
 
-void updateCourseIf(ListCourses& List) {
+void updateCourseInfo(ListCourses& List) {
 	string Id;
-	cout << "Nhap ma mon hoc de cap nhat thong tin : ";
+	cin.ignore();
+	cout << "Nhap id mon hoc de cap nhat thong tin : ";
 	getline(cin, Id);
+	if (List.head->id == Id) {
+		cout << "Ban muon cap nhat thong tin nao " << endl;
+		cout << "\t1.Cap nhat ma mon hoc. \n";
+		cout << "\t2.Cap nhat ten mon hoc.\n ";
+		cout << "\t3.Cap nhat ten giao vien.\n";
+		int chon = 0; cin >> chon;
+		cin.ignore();
+		if (chon == 1) {
+			cout << "\t\tNhap ma lop moi : "; getline(cin, List.head->id);
+		}
+		else if (chon == 2) {
+			cout << "\t\tNhap ten moc hoc moi : "; getline(cin, List.head->courseName);
+		}
+		else if (chon == 3) {
+			cout << "\t\tNhap ten giao vien moi : "; getline(cin, List.head->teacherName);
+		}
+		return;
+	}
 	Course* p = List.head;
 	while (p != NULL) {
 		if (p->id == Id) {
@@ -102,6 +271,7 @@ void updateCourseIf(ListCourses& List) {
 			cout << "\t2.Cap nhat ten mon hoc.\n ";
 			cout << "\t3.Cap nhat ten giao vien.\n";
 			int chon = 0; cin >> chon;
+			cin.ignore();
 			if (chon == 1) {
 				cout << "\t\tNhap ma lop moi : "; getline(cin, p->id);
 			}
@@ -116,23 +286,77 @@ void updateCourseIf(ListCourses& List) {
 	}
 
 }
+void fileListStudent(Course* x, Student *a) {
+	if (x == NULL) {
+		return;
+	}
+	ofstream file("./Course/" + x->id + ".csv",ios::out|ios::app);
+	file << a->studentID << "," << a->lastName << "," << a->firstName << "," << a->dateOfBirth.ngay << "/" << a->dateOfBirth.thang << "/" << a->dateOfBirth.nam << endl;
 
-void addStudentToCourse(Student& a, Course* x) {
+	//file.close();
+}
+void addStudentToCourse(Student*&a, Course* x) {
 	if (x == NULL) {
 		cout << "Mon hoc ko ton tai " << endl;
 		return;
 	}
-	if (a.enrolledCourses.head == NULL) {
-		a.enrolledCourses.head = a.enrolledCourses.tail = x;
+	if (a->enrolledCourses.head == NULL) {
+		a->enrolledCourses.head = a->enrolledCourses.tail = x;
+		fileListStudent(x,a);
 		return;
 	}
-	Course* p = a.enrolledCourses.head;
+	Course* p = a->enrolledCourses.head;
 	while (1) {
 		if (p->next == NULL) break;
 		p = p->next;
 	}
 	p->next = x;
+	fileListStudent(x, a);
 }
+
+ListStudent CreateListStudent() {
+	ListStudent l;
+	l.head = l.tail = NULL;
+	return l;
+}
+
+int main() {
+	Semester HocKi;
+	bool Run = true;
+	while (Run) {
+		cout << "1.Create a semester\n";
+		cout << "2.Add a course to this semester\n";
+		cout << "3.View list of course\n";
+		cout << "4.Update coures infomartion\n";
+		cout << "5.Add a student to the course\n";
+		cout << "0.end\n";
+		int chon = 0; cin >> chon;
+		if (chon == 1) {
+			HocKi = createSemester();
+		}
+		else if (chon == 2) {
+			addCourseToSemester(HocKi);
+		}
+		else if (chon == 3) {
+			viewOfCourse(HocKi.list);
+		}
+		else if (chon == 4) {
+			updateCourseInfo(HocKi.list);
+		}
+		else if (chon == 0) {
+			Run = false;
+		}
+		else if (chon == 5) {
+			Student* sinhVien = khoitaosinhvien();
+			addStudentToCourse(sinhVien, HocKi.list.head);
+		}
+
+	}
+	return 0;
+}
+
+
+
 
 
 

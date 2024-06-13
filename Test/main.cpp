005d9e0faf65bc137f8d24a642b369b8e08e6bcd
QUỐC NGUYỄN KIM <23120347@student.hcmus.edu.vn>
2024-06-13 19:17:30 +0700

    Them ham

--- Test/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "global.h"
-int main()
-{
-	cout << "Chuc ban may man \n";
-	return 0;
-}
---
+++ Test11/main.cpp
@@ -0,0 +1,7 @@
+#include "global.h"
+
+int main()
+{
+	cout << "Chuc ban may man \n";
+	return 0;
+}
